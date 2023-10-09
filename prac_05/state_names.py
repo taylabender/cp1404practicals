2a56dced4ca7619ec84e1286cca91e88e8907003
taylabender <tayla.bender@my.jcu.edu.au>
2023-10-09 12:52:55 +1000

    RAW state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,7 +4,7 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+# ToDO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
             "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
